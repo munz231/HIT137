b2d0a9e7ffc17b29c18a499e0651874663aaebcd
icheworei <133112992+icheworei@users.noreply.github.com>
2024-01-17 00:06:32 +0930

    Update HIT137 Assignment.py

--- HIT137 Assignment.py
+++ HIT137 Assignment.py
@@ -4,15 +4,7 @@ for i in range (5):
                 total+=i+j
             else:
                 total+=i-j
-counter = 0
 
-while counter <5:
-    if total <13:
-        total+=1
-    elif total >13:
-        total=1
-    else:
-        counter += 2
             
               
       
