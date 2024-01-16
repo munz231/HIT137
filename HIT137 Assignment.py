b707d3986b98a761db95fc6f27b5965d97893a9c
munz231 <150678734+munz231@users.noreply.github.com>
2024-01-17 00:29:21 +0930

    Update HIT137 Assignment.py

--- HIT137 Assignment.py
+++ HIT137 Assignment.py
@@ -1,10 +1,1 @@
-for i in range (5):
-        for j in range (3):
-            if i+j==5:
-                total+=i+j
-            else:
-                total+=i-j
 
-            
-              
-      
